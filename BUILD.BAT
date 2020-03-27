d48d24c8893e437763988b1b9a6323e5a416ac03
asie <kontakt@asie.pl>
2020-03-27 20:22:29 +0100

    add error checking to BUILD.BAT (thanks zzo38), minor consistency cleanup

--- BUILD.BAT
+++ BUILD.BAT
@@ -9,17 +9,20 @@ echo.
 echo [ Building DATPACK.EXE ]
 echo.
 TPC /B /EBIN TOOLS\DATPACK.PAS
+if errorlevel 1 goto error
 echo.
 echo [ Building ZZT.DAT ]
 echo.
 cd DOC
 ..\BIN\DATPACK.EXE /C ..\BIN\ZZT.DAT *.*
+if errorlevel 1 goto error
 cd ..
 
 echo.
 echo [ Building ZZT.EXE ]
 echo.
 TPC /B /EBIN /GD /ISRC /USRC SRC\ZZT.PAS
+if errorlevel 1 goto error
 echo.
 echo [ Compressing ZZT.EXE ]
 echo.
@@ -36,4 +39,8 @@ copy BIN\ZZT.DAT DIST\ZZT.DAT
 copy RES\ZZT.CFG DIST\ZZT.CFG
 copy RES\DEMO.ZZT DIST\DEMO.ZZT
 copy LICENSE.TXT DIST\LICENSE.TXT
-
+goto done
+:error
+echo.
+echo [ Error detected! Stopping. ]
+:done
